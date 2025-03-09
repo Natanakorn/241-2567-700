37cf946b87f6e24eac78daca583ca94c328bd5b3
Natanakorn <natanakorn.c@ku.th>
2025-03-09 12:51:41 +0700

    Natanakorn 3/9/25

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-function submitData () {
+const validateData = (userData) => {
+    let errors =[]
+
+    if (!userData.firstname ){
+        errors.push('กรุณากรอกชื่อ')
+    }
+    if (!userData.lastname ){
+        errors.push('กรุณากรอกนามสกุล')
+    }
+    if (!userData.age ){
+        errors.push('กรุณากรอกอายุ')
+    }
+    if (!userData.gender){
+        errors.push('กรุณาเลือกเพศ')
+    }
+    if (!userData.interests){
+        errors.push('กรุณาเลือกความสนใจ')
+    }
+    if (!userData.description){
+        errors.push('กรุณากรอกคำอธิบาย')
+    }
+    return errors
+}
+
+const submitData = async () => {
     let firstNameDOM = document.querySelector('input[name=firstname]');
     let lastNameDOM = document.querySelector('input[name=lastname]');
     let ageDOM = document.querySelector('input[name=age]');
@@ -6,6 +30,9 @@ function submitData () {
     let interestDOM = document.querySelectorAll('input[name=interest]:checked');
     let descriptionDOM = document.querySelector('textarea[name=description]');
 
+    let messageDOM = document.getElementById('.message');
+    
+    try{
     let interest = ''
     for (let i = 0; i < interestDOM.length; i++){
         interest += interestDOM[i].value 
@@ -21,8 +48,47 @@ function submitData () {
         age: ageDOM.value,
         gender: genderDOM.value,
         description: descriptionDOM.value,
-        interest: interest
-        
+        interests: interest   
     }
     console.log('submitData', userData);
-}
+/*
+        const errors = validateData(userData)
+
+        if (errors.length > 0){
+            //มีerror
+            throw {
+                message: 'กรุณากรอกข้อมูลให้ครบถ้วน',
+                errors: errors
+            }
+        }
+*/
+    
+        const response = await axios.post('http://localhost:8000/users', userData)
+        console.log('responsse', response.data);
+        messageDOM.innerText = 'บันทึกข้อมูลเรียบร้อย'
+        messageDOM.className = 'message success'
+    } catch (error){
+        console.log('error message', error.message);
+        console.log('error', error.errors);
+        
+        if (error.response){
+            console.log('error', error.response.data.message);
+            error.message = error.response.data.message
+            error.errors = error.response.data.errors
+        }
+        
+
+        let htmlData = '<div>'
+        htmlData += `<div> ${error.message} </div>` 
+        htmlData += '<ul>'
+        for(let i = 0; i < error.errors.length; i++){
+            htmlData += `<li>${error.errors[i]}</li>`
+        }
+        htmlData += '</ul>'
+        htmlData += '</div>'
+        
+        
+        messageDOM.innerText = 'บันทึกข้อมูลไม่สำเร็จ'
+        messageDOM.className = 'message danger'
+        }
+    }
